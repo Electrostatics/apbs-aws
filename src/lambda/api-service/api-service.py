b4cd9d367898124f194fe00796ae607d088bd867
Elvis Offor <eoffor21@gmail.com>
2021-02-15 15:34:19 -0800

    Create and return URL with S3 token

--- src/lambda/api-service/api-service.py
+++ src/lambda/api-service/api-service.py
@@ -1,11 +1,28 @@
 from string import ascii_lowercase, digits
 from random import choices
+from typing import List
+import os
+import boto3
+
+INPUT_BUCKET_NAME = os.getenv('INPUT_BUCKET_NAME', 'sample_bucket')
 
 def create_s3_url(file_name, prefix_name):
     # TODO: 2021/02/11, Elvis - Use boto3 to create and return presigned URL for return
-    return 'This is an S3 token'    
+    object_name = f'{prefix_name}/{file_name}'
+    s3_client = boto3.client('s3')
 
-def generate_id_and_token(file_name, job_id=None):
+    url = s3_client.generate_presigned_url( 'put_object',
+                                            Params={
+                                                'Bucket': INPUT_BUCKET_NAME,
+                                                'Key': object_name
+                                            },
+                                            ExpiresIn=3600,
+                                          )
+    
+    return url
+
+# TODO 2021/02/15 - Add support for asking for multiple tokens at once
+def generate_id_and_token(file_name: str, job_id:str=None):
 
     # Generate new job ID if not provided
     if job_id is None:
