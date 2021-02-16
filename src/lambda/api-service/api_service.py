0a29692f2c08ba05e6adc6d0e69f954bb70b564f
Elvis Offor <eoffor21@gmail.com>
2021-02-15 16:59:16 -0800

    Use bucket name argument instead of environment

--- src/lambda/api-service/api_service.py
+++ src/lambda/api-service/api_service.py
@@ -1,27 +1,23 @@
 from string import ascii_lowercase, digits
 from random import choices
 from typing import List
-import os
 import boto3
 
-INPUT_BUCKET_NAME = os.getenv('INPUT_BUCKET_NAME', 'sample_bucket')
-
-def create_s3_url(file_name, prefix_name):
-    # TODO: 2021/02/11, Elvis - Use boto3 to create and return presigned URL for return
+def create_s3_url(bucket_name: str, file_name: str, prefix_name: str) -> str:
     object_name = f'{prefix_name}/{file_name}'
     s3_client = boto3.client('s3')
 
+    # Generate presigned URL for file
     url = s3_client.generate_presigned_url( 'put_object',
                                             Params={
-                                                'Bucket': INPUT_BUCKET_NAME,
+                                                'Bucket': bucket_name,
                                                 'Key': object_name
                                             },
                                             ExpiresIn=3600,
                                           )
-    
     return url
 
-def generate_id_and_tokens(file_list: List[str], job_id:str=None):
+def generate_id_and_tokens(bucket_name: str, file_list: List[str], job_id:str=None) -> dict:
 
     # Generate new job ID if not provided
     if job_id is None:
@@ -30,7 +26,7 @@ def generate_id_and_tokens(file_list: List[str], job_id:str=None):
     # Create URLs with S3 tokens
     url_dict = {}
     for file_name in file_list:
-        token_url = create_s3_url(file_name, job_id)
+        token_url = create_s3_url(bucket_name, file_name, job_id)
         url_dict[file_name] = token_url
 
     # Generate JSON response
