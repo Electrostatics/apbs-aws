40815d9497caaa4098b4d2dbb376d03b4f7525fc
intendo <intendo23@gmail.com>
2021-05-05 11:46:02 -0700

    Refactor - add methods for simplicity

--- lambda_services/job_service/launcher/pdb2pqr_runner.py
+++ lambda_services/job_service/launcher/pdb2pqr_runner.py
@@ -29,10 +29,7 @@ class Runner(JobSetup):
                     "invoke_method found, value: %s",
                     str(form["invoke_method"]),
                 )
-                if (
-                    form["invoke_method"].lower() == "v2"
-                    or form["invoke_method"].lower() == "cli"
-                ):
+                if form["invoke_method"].lower() in ["v2", "cli"]:
                     self.invoke_method = "cli"
                     self.cli_params = {
                         "pdb_name": form["pdb_name"],
@@ -40,10 +37,7 @@ class Runner(JobSetup):
                         "flags": form["flags"],
                     }
 
-                elif (
-                    form["invoke_method"].lower() == "v1"
-                    or form["invoke_method"].lower() == "gui"
-                ):
+                elif form["invoke_method"].lower() in ["v1", "gui"]:
                     self.invoke_method = "gui"
                     self.weboptions = WebOptions(form)
             else:
@@ -64,86 +58,91 @@ class Runner(JobSetup):
     def prepare_job(self):
         job_id = self.job_id
 
-        if self.invoke_method == "gui" or self.invoke_method == "v1":
-
-            # Retrieve information about the
-            #   PDB fileand command line arguments
-            if self.weboptions.user_did_upload:
-                # Update input files
-                self.add_input_file(self.weboptions.pdbfilename)
-
-            else:
-                if os.path.splitext(self.weboptions.pdbfilename)[1] != ".pdb":
-                    self.weboptions.pdbfilename = (
-                        self.weboptions.pdbfilename + ".pdb"
-                    )  # add pdb extension to pdbfilename
-
-                    # Add url to RCSB PDB file to input file list
-                    self.add_input_file(
-                        f"https://files.rcsb.org/download/{self.weboptions.pdbfilename}"
-                    )
-
-            # Check for userff, names, ligand files to add to input_file list
-            if hasattr(self.weboptions, "ligandfilename"):
-                self.add_input_file(self.weboptions.ligandfilename)
-            if hasattr(self.weboptions, "userfffilename"):
-                self.add_input_file(self.weboptions.userfffilename)
-            if hasattr(self.weboptions, "usernamesfilename"):
-                self.add_input_file(self.weboptions.usernamesfilename)
-
-            # Make the pqr name prefix the job_id
-            self.weboptions.pqrfilename = job_id + ".pqr"
-
-            # Retrieve PDB2PQR command line arguments
-            command_line_args = self.weboptions.get_command_line()
-            if "--summary" in command_line_args:
-                command_line_args = command_line_args.replace("--summary", "")
-
-            logging.debug(command_line_args)
-            logging.debug(self.weboptions.pdbfilename)
-
-        elif self.invoke_method == "cli" or self.invoke_method == "v2":
-            # construct command line argument string for when CLI is invoked
-            command_line_list = []
-
-            # Add PDB filename to input file list
-            self.add_input_file(self.cli_params["pdb_name"])
-
-            # get list of args from self.cli_params['flags']
-            for name in self.cli_params["flags"]:
-                command_line_list.append(
-                    (name, self.cli_params["flags"][name])
-                )
-
-                # Add to input file list if userff, names,
-                #  or ligand flags are defined
-                if (
-                    name in ["userff", "usernames", "ligand"]
-                    and self.cli_params[name]
-                ):
-                    self.add_input_file(self.cli_params[name])
-
-            command_line_args = ""
-
-            # append to command_line_str
-            for pair in command_line_list:
-                # TODO: add conditionals later to
-                #       distinguish between data types
-                if isinstance(pair[1], bool):
-                    cli_arg = "--%s" % (pair[0])
-                else:
-                    cli_arg = "--%s=%s" % (
-                        pair[0],
-                        str(pair[1]),
-                    )
-                command_line_args = "%s %s" % (command_line_args, cli_arg)
-
-            # Add PDB and PQR file names to command line string
-            command_line_args = "%s %s %s" % (
-                command_line_args,
-                self.cli_params["pdb_name"],
-                self.cli_params["pqr_name"],
-            )
-
+        if self.invoke_method in ["gui", "v1"]:
+            command_line_args = self.version_1_job(job_id)
+        elif self.invoke_method in ["cli", "v2"]:
+            command_line_args = self.version_2_job()
         self.command_line_args = command_line_args
         return command_line_args
+
+    def version_2_job(self):
+        # construct command line argument string for when CLI is invoked
+        command_line_list = []
+
+        # Add PDB filename to input file list
+        self.add_input_file(self.cli_params["pdb_name"])
+
+        # get list of args from self.cli_params['flags']
+        for name in self.cli_params["flags"]:
+            command_line_list.append((name, self.cli_params["flags"][name]))
+
+            # Add to input file list if userff, names,
+            #  or ligand flags are defined
+            if (
+                name in ["userff", "usernames", "ligand"]
+                and self.cli_params[name]
+            ):
+                self.add_input_file(self.cli_params[name])
+
+        result = ""
+
+        # append to command_line_str
+        for pair in command_line_list:
+            # TODO: add conditionals later to
+            #       distinguish between data types
+            if isinstance(pair[1], bool):
+                cli_arg = "--%s" % (pair[0])
+            else:
+                cli_arg = "--%s=%s" % (
+                    pair[0],
+                    str(pair[1]),
+                )
+            result = "%s %s" % (result, cli_arg)
+
+            # Add PDB and PQR file names to command line string
+        result = "%s %s %s" % (
+            result,
+            self.cli_params["pdb_name"],
+            self.cli_params["pqr_name"],
+        )
+
+        return result
+
+    def version_1_job(self, job_id):
+        # Retrieve information about the
+        #   PDB fileand command line arguments
+        if self.weboptions.user_did_upload:
+            # Update input files
+            self.add_input_file(self.weboptions.pdbfilename)
+        else:
+            if os.path.splitext(self.weboptions.pdbfilename)[1] != ".pdb":
+                self.weboptions.pdbfilename = (
+                    self.weboptions.pdbfilename + ".pdb"
+                )  # add pdb extension to pdbfilename
+
+                # Add url to RCSB PDB file to input file list
+                self.add_input_file(
+                    f"https://files.rcsb.org/download/"
+                    f"{self.weboptions.pdbfilename}"
+                )
+
+        # Check for userff, names, ligand files to add to input_file list
+        if hasattr(self.weboptions, "ligandfilename"):
+            self.add_input_file(self.weboptions.ligandfilename)
+        if hasattr(self.weboptions, "userfffilename"):
+            self.add_input_file(self.weboptions.userfffilename)
+        if hasattr(self.weboptions, "usernamesfilename"):
+            self.add_input_file(self.weboptions.usernamesfilename)
+
+        # Make the pqr name prefix the job_id
+        self.weboptions.pqrfilename = job_id + ".pqr"
+
+        # Retrieve PDB2PQR command line arguments
+        result = self.weboptions.get_command_line()
+        if "--summary" in result:
+            result = result.replace("--summary", "")
+
+        logging.debug(result)
+        logging.debug(self.weboptions.pdbfilename)
+
+        return result
