5e6ada82ffb678471c4a96eeeb97372be8913b20
Elvis Offor <eoffor21@gmail.com>
2021-06-21 09:20:27 -0700

    Rearranged build operation order

--- build_and_update.sh
+++ build_and_update.sh
@@ -21,12 +21,12 @@ build_api_service() {
 build_job_service() {
     echo "Updating Lambda function (Job Service)"
     cd $CODEBUILD_SRC_DIR/lambda_services; zip -r /tmp/job.zip job_service
-    aws lambda update-function-code --function-name apbs-$IMAGE_TAG-job-L --publish --zip-file fileb:///tmp/job.zip    
+    aws lambda update-function-code --function-name apbs-$IMAGE_TAG-job-L --publish --zip-file fileb:///tmp/job.zip
 }
 
 build_docker() {    
     echo "Building the Docker image..."
-    docker build -t $IMAGE_REPO_NAME:main.base src/docker
+    docker build -t $IMAGE_REPO_NAME:main.base $CODEBUILD_SRC_DIR/src/docker
     docker tag $IMAGE_REPO_NAME:main.base    $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_REPO_NAME:$IMAGE_TAG
 }
 
@@ -39,10 +39,10 @@ build_changed() {
 
     git diff --name-only $CODEBUILD_RESOLVED_SOURCE_VERSION $LAST_SUCCESSFUL_COMMIT_ID | while read file_path || [[ -n $file_path ]];
     do
-        if [[ $file_path == lambda_services/api_service/* ]] && [[ $api_service_changed -ne 1 ]] # condition to check if path in ./lambda_services/api_service/ directory
+        if [[ $file_path == src/docker/* ]] && [[ $docker_changed -ne 1 ]]
         then
-            api_service_changed=1
-            build_api_service
+            docker_changed=1
+            build_docker
         fi
 
         if [[ $file_path == lambda_services/job_service/* ]] && [[ $job_service_changed -ne 1 ]]
@@ -51,19 +51,19 @@ build_changed() {
             build_job_service
         fi
 
-        if [[ $file_path == src/docker/* ]] && [[ $docker_changed -ne 1 ]]
+        if [[ $file_path == lambda_services/api_service/* ]] && [[ $api_service_changed -ne 1 ]] # condition to check if path in ./lambda_services/api_service/ directory
         then
-            docker_changed=1
-            build_docker
+            api_service_changed=1
+            build_api_service
         fi
     done
 }
 
 build_all() {
     echo "Building all services and containers"
-    build_api_service
-    build_job_service
     build_docker
+    build_job_service
+    build_api_service
 }
 
 cli_argument="$1"
