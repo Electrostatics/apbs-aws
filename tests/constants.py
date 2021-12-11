163c8e9683ccfa69d1040d84d98bdf4251ad92a3
Elvis Offor <eoffor21@gmail.com>
2021-12-10 19:52:19 -0800

    Finish test for generating APBS input file

--- tests/constants.py
+++ tests/constants.py
@@ -1,5 +1,5 @@
 from pathlib import Path
 
-# DATA_DIR = Path(__file__).parent.absolute() / "data"
-REF_DIR = Path("expected_data")
-INPUT_DIR = Path("input_data")
+DATA_DIR = Path(__file__).parent.absolute()  # / "data"
+REF_DIR = DATA_DIR / Path("expected_data")
+INPUT_DIR = DATA_DIR / Path("input_data")
--- tests/test_apbs_setup.py
+++ tests/test_apbs_setup.py
@@ -1,31 +1,38 @@
-
+"""This file tests functions for setting up APBS jobs."""
+from json import load
 from pathlib import Path
 import pytest
 
+from .constants import INPUT_DIR, REF_DIR
+from lambda_services.job_service.launcher.apbs_runner import Runner
 from lambda_services.job_service.launcher.utils import apbs_infile_creator
-from .constants import REF_DIR
 
 
 @pytest.mark.parametrize(
-    "expected_infile_path,",
+    "pqr_file_name, form_input_filename, expected_infile_name,",
     [
-        pytest.param("1fas-default.in", id="1fas-default"),
-        pytest.param("1fas-ion.in", id="1fas-ion"),
+        pytest.param("1fas.pqr", "test_apbs_setup-1fas-default.json", "1fas-default.in", id="1fas-default"),
+        pytest.param("1fas.pqr", "test_apbs_setup-1fas-ion.json", "1fas-ion.in", id="1fas-ion"),
     ],
 )
-def test_apbs_infile_creator(tmp_path, expected_infile_name: str):
+def test_apbs_infile_creator(pqr_file_name: str, form_input_filename: str, expected_infile_name: str):
 
-    job_tag: str
-    apbs_options: dict
+    # Get path information
     expected_path: Path = REF_DIR / Path(expected_infile_name)
+    form_path: Path = INPUT_DIR / Path(form_input_filename)
 
-    # TODO: create dummy job_tag
-    job_tag = ""
-    # TODO: create dummy apbs_options
-    apbs_options = {}
+    # Set sample job information
+    job_id: str = "sampleId"
+    job_date: str = "2021-05-16"
+    job_tag: str = f"{job_date}/{job_id}"
 
-    # create input file from options
-    new_infile_contents = apbs_infile_creator(job_tag, apbs_options)
+    # Load input data
+    form_data: dict = load(open(form_path))
+    apbs_options: Runner = Runner(form_data, job_id, job_date).apbs_options
+    apbs_options["pqrFileName"] = pqr_file_name
 
-    # Get contents of expected file and compare
+    # Generate input file contents
+    new_infile_contents: str = apbs_infile_creator(job_tag, apbs_options)
+
+    # Compare contents with reference file
     assert new_infile_contents == open(expected_path, 'r').read()
